b782bfc167f9ab69f6eecad21b96eeab5253935c
ngduran <norbertogarciaduran@gmail.com>
2024-12-24 15:17:09 -0300

    Adicionando props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
   
@@ -10,7 +12,12 @@ function App() {
     <div className="App">
       <h2>Alterando o JSX</h2>
       <p>Olá, {name}</p>
-      <HelloWorld />      
+      <HelloWorld />
+      <SayMyName nome='Norberto'/>
+      <SayMyName nome='Keyla'/>
+      <SayMyName nome={name}/>
+      <Pessoa nome="Norberto" idade="50" profissao="Programador" foto="https://via.placeholder.com/150" />
+
     </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,11 @@
+function SayMyName(props) {
+
+    return (
+        <div>
+            <p>Fala aí {props.nome}, tudo bem!</p>
+        </div>
+    );
+
+}
+
+export default SayMyName;
